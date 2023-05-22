18424c63b018ead8c56177a08da4b541ba2777c5
satishrajugithub <123757426+satishrajugithub@users.noreply.github.com>
2023-05-22 07:01:23 +0530

    Update DockerApp.java

--- src/main/java/com/levo/dockerexample/DockerApp.java
+++ src/main/java/com/levo/dockerexample/DockerApp.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DockerApp {
 	public static void main(String[] args) {
+		System.out.println("Hi Satish this is from sprint boot java app");
 		SpringApplication.run(DockerApp.class, args);
 	}
 }
